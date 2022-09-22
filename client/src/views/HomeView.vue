1c7ef5d1d2a9141075ce3a8610b4a2d11dd2f4ad
Daniel Murphy <knysnacoder@gmail.com>
2022-09-22 09:34:12 +0200

    Setting up the map tilelayer

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="home">
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { onMounted } from 'vue';
+import leaflet from 'leaflet'
 
 export default {
   name: 'HomeView',
   components: {
+  },
+  setup() {
+    let map;
+    onMounted(() => {
+      map = leaflet.map('map').setView([51.505, -0.09], 9);
+      leaflet.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
+        {
+          attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          tileSize: 512,
+          zoomOffset: -1
+        }).addTo(map);
+    })
   }
 }
 </script>
