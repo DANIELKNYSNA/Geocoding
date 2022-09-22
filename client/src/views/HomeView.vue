e092dcf6f6198ee0b7c925fb3dfc04771584a872
Daniel Murphy <knysnacoder@gmail.com>
2022-09-22 10:01:44 +0200

    Adding functionality

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,7 +6,7 @@
 
 <script>
 // @ is an alias to /src
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import leaflet from 'leaflet'
 
 export default {
@@ -16,14 +16,50 @@ export default {
   setup() {
     let map;
     onMounted(() => {
-      map = leaflet.map('map').setView([51.505, -0.09], 9);
+      map = leaflet.map('map').setView([-34.0553076, 23.0329035], 9);
       leaflet.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
         {
           attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
           tileSize: 512,
           zoomOffset: -1
         }).addTo(map);
+      getGeoLocation()
     })
+
+    const coords = ref(null)
+    const fetchCoords = ref(null)
+    const geoMarker = ref(null)
+
+    const getGeoLocation = () => {
+      fetchCoords.value = true
+      navigator.geolocation.getCurrentPosition(setCoords, getLocError)
+
+    }
+    const setCoords = (pos) => {
+      fetchCoords.value = null
+      // Store the coords in sessionStorage
+      const setSessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      }
+      sessionStorage.setItem('coords', JSON.stringify(setSessionCoords))
+
+      // Now set the coords value to the ref defined above
+      coords.value = setSessionCoords
+
+      //Now call the function to plot the location on the map
+      plotGeoLocation(coords)
+
+
+    }
+    const getLocError = (error) => {
+      console.log(error)
+    }
+
+    const plotGeoLocation = () => {
+
+    }
+    return { coords, geoMarker, getLocError }
   }
 }
 </script>
